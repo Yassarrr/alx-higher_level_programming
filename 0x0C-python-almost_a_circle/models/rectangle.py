af2a236b69b1afa5814c2fa925c0195a67a09eaa
Abdulyassar Murtala <abdulyassarmurtala@gmail.com>
2022-10-07 12:03:51 +0100

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,55 +1,123 @@
 #!/usr/bin/python3
-''' First rectangle
-'''
-
+"""
+This module implements a Rectangle object
+"""
 from models.base import Base
 
 
 class Rectangle(Base):
-    '''class Rectangle inherits from base
-    '''
-    KV_dict = {'id': 'id', 'width': '_Rectangle__width',
-               'height': '_Rectangle__height',
-               'x': '_Rectangle__x', 'y': '_Rectangle__y'}
+    """Rectangle implementation
+    """
 
-    def __init__(self, width, height, x=0, y=0, id=None):
-        '''method __init__ Initialization a Rectangle
-        '''
+    def __init__(self, width: int, height: int, x=0, y=0, id=None):
+        """initialization
+        """
         super().__init__(id)
+
         self.width = width
         self.height = height
         self.x = x
         self.y = y
 
-    def update(self, *args, **kwargs):
-        '''method update
-        '''
-        key_list = ['id', '_Rectangle__width', '_Rectangle__height',
-                    '_Rectangle__x', '_Rectangle__y']
-        KV_dict = {'id': 'id', 'width': '_Rectangle__width',
-                   'height': '_Rectangle__height',
-                   'x': '_Rectangle__x', 'y': '_Rectangle__y'}
-        for idx, el in enumerate(args):
-            self.__dict__[key_list[idx]] = el
-        if len(args) == 0:
-            for key, val in kwargs.items():
-                self.__dict__[KV_dict[key]] = val
+    def __str__(self) -> str:
+        """string representation
+        """
+        return "[Rectangle] ({}) {}/{} - {}/{}" \
+            .format(self.id, self.x, self.y, self.width, self.height)
 
-    def __str__(self):
-        '''method __str__
-        '''
+    def check_type_value(self, name:  str, value: object, greater_equal=False):
+        """type and value validation
+        """
 
-        return ("[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x,
-                self.__y, self.__width, self.__height))
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if not greater_equal:
+            if value <= 0:
+                raise ValueError("{} must be > 0".format(name))
+        else:
+            if value < 0:
+                raise ValueError("{} must be >= 0".format(name))
 
-    def area(self):
-        '''public_method area of rectangle
-        '''
-        return self.__width * self.__height
+    @property
+    def width(self) -> int:
+        """width getter
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, width: int):
+        """width setter
+        """
+        self.check_type_value('width', width)
+        self.__width = width
+
+    @property
+    def height(self) -> int:
+        """height getter
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, height: int):
+        """height setter
+        """
+        self.check_type_value('height', height)
+        self.__height = height
+
+    @property
+    def x(self) -> int:
+        """x getter
+        """
+        return self.__x
+
+    @x.setter
+    def x(self, x: int):
+        """x setter
+        """
+        self.check_type_value('x', x, True)
+        self.__x = x
+
+    @property
+    def y(self) -> int:
+        """y getter
+        """
+        return self.__y
+
+    @y.setter
+    def y(self, y: int):
+        """y setter
+        """
+        self.check_type_value('y', y, True)
+        self.__y = y
+
+    def area(self) -> int:
+        """area
+        """
+        return self.width * self.height
 
     def display(self):
-        '''public method display self prints in stdout #
-        '''
-        print("\n" * (self.__y), end="")
-        for i in range(self.__height):
-            print(" " * self.__x + "#" * self.__width)
+        """prints # shape of the rectangle
+        """
+        print('\n'*self.y, end='')
+        for l in range(self.height):
+            print(' '*self.x + '#'*self.width)
+
+    def update(self, *args, **kwargs):
+        """update rectangle attributes
+        """
+
+        expect = (self.id, self.width, self.height, self.x, self.y)
+        if args != ():
+            self.id, self.width, self.height, self.x, self.y = \
+                args + expect[len(args):len(expect)]
+        elif kwargs:
+            for (name, value) in kwargs.items():
+                setattr(self, name, value)
+
+    def to_dictionary(self) -> int:
+        """rectangle to dictionary
+        """
+
+        return {
+            'x': self.x, 'y': self.y, 'id': self.id,
+            'height': self.height, 'width': self.width}
